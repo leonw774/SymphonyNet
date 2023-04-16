69a19562398476c9c8375ae2ab656a3063d26dca
leonw774 <leonw774@gmail.com>
2023-04-16 19:37:14 +0800

    ok turns out note onset limit is necessary

--- src/preprocess/preprocess_midi.py
+++ src/preprocess/preprocess_midi.py
@@ -129,6 +129,7 @@ def limit_max_track(p_midi, MAX_TRACK=40):  # merge track with least notes and l
 
     p_midi.instruments = good_instruments
 
+NOTE_ONSET_LIMIT = 0xFFFFFFF
 
 def get_init_note_events(p_midi):  # extract all notes in midi file
 
@@ -136,6 +137,7 @@ def get_init_note_events(p_midi):  # extract all notes in midi file
     for track_idx, instrument in enumerate(p_midi.instruments):
         # track_idx_lst.append(track_idx)
         for note in instrument.notes:
+            assert note.start <= NOTE_ONSET_LIMIT or note.end <= NOTE_ONSET_LIMIT, 'note.start/end onset too large'
             note_dur = note.end - note.start
             if note_dur < 0:
                 continue
