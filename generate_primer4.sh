41667f22eb91a379733611770557174988ee8c0f
leonw774 <leonw774@gmail.com>
2023-06-08 23:19:49 +0800

    Update generate_primer4.sh

--- generate_primer4.sh
+++ generate_primer4.sh
@@ -1,7 +1,8 @@
 test_pathlist=$1
+midis_dir_path=$2
 start_time=$SECONDS
-while read eval_sample_midi_path; do
-   primer_name=$(basename "$eval_sample_midi_path" .mid)
-   python generate_uncond.py -p "../MyMidiModel/${eval_sample_midi_path}" -m 4 -l 4096 -o "generated/${primer_name}"
+while read test_midi_path; do
+   primer_name=$(basename "$test_midi_path" .mid)
+   python generate_uncond.py -p "${midis_dir_path}/${test_midi_path}" -m 4 -l 4096 -o "generated/${primer_name}"
 done < $eval_pathlist_path
 duration=$(( $SECONDS - start_time ))
