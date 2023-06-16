736652309ffcad1e9c8f4be7ec56190bfaefcbac
leonw774 <leonw774@gmail.com>
2023-06-16 19:57:27 +0800

    add checkpoint arg for generation script

--- generate_primer4.sh
+++ generate_primer4.sh
@@ -1,8 +1,9 @@
 test_pathlist=$1
 midis_dir_path=$2
+checkpoint_path=$3
 start_time=$SECONDS
 while read test_midi_path; do
    primer_name=$(basename "$test_midi_path" .mid)
-   python generate_uncond.py -p "${midis_dir_path}/${test_midi_path}" -m 4 -l 4096 -o "generated/${primer_name}"
+   python generate_uncond.py -p "${midis_dir_path}/${test_midi_path}" -m 4 -l 4096 -o "generated/${primer_name}" -c $checkpoint_path
 done < $eval_pathlist_path
 duration=$(( $SECONDS - start_time ))
--- generate_uncond.sh
+++ generate_uncond.sh
@@ -1,5 +1,6 @@
 test_pathlist=$1
 sample_number=$(wc -l < $test_pathlist)
+checkpoint_path=$2
 start_time=$SECONDS
-python generate_uncond.py -n $sample_number -m 0 -l 4096 -o "generated/uncond"
+python generate_uncond.py -n $sample_number -m 0 -l 4096 -o "generated/uncond" -c $checkpoint_path
 duration=$(( $SECONDS - start_time ))
