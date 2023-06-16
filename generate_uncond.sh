bb1f7d8c562c1016b14bd08dadffc6c5f1d27445
leonw774 <leonw774@gmail.com>
2023-06-16 16:06:07 +0800

    Update generate_uncond.sh

--- generate_uncond.sh
+++ generate_uncond.sh
@@ -1,5 +1,5 @@
 test_pathlist=$1
 sample_number=$(wc -l < $test_pathlist)
 start_time=$SECONDS
-python generate_uncond.py -n $sample_number -m 4 -l 4096 -o "generated/uncond"
+python generate_uncond.py -n $sample_number -m 0 -l 4096 -o "generated/uncond"
 duration=$(( $SECONDS - start_time ))
