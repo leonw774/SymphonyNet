9dd96509c063c4b48057da390b086c013a33551b
leonw774 <leonw774@gmail.com>
2023-06-25 08:46:26 +0800

    Update generate_uncond.sh

--- generate_uncond.sh
+++ generate_uncond.sh
@@ -3,7 +3,7 @@ sample_number=$(wc -l < $test_pathlist)
 checkpoint_path=$2
 start_time=$SECONDS
 log_path=$(date '+%Y%m%d-%H%M%S')-generate_uncond.log
-for i in (1 .. $sample_number) do
+for i in {1 .. $sample_number} do
     python generate_uncond.py -n 1 -m 0 -l 4096 -o "generated/uncond" -c $checkpoint_path --use-cuda | tee -a $log_path
 done
 duration=$(( $SECONDS - start_time ))
